abd6fa375955a22d26ef434109cfa07a1ab24634
nico <nicomicu8@gmail.com>
2024-09-26 14:56:31 -0300

    remove validationError

--- src/controllers/price.controller/index.js
+++ src/controllers/price.controller/index.js
@@ -1,14 +1,11 @@
 const db = require("../../models/index")
 const { Price } = db
-const { AppError, ValidationError } = require("../../errors/crateErrorFactory")
 
 exports.getPrices = async (req, res, next) => {
   try {
     const prices = await Price.findAll({
       attributes: ["id", "amount"],
     })
-
-    throw new ValidationError("Error precios", 400)
 
     res.status(200).send(prices)
   } catch (error) {
